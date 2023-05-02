40bd3c2681b2a77069a890c6a5b869d1c0895950
Vlas900 <gnetovvlad@gmail.com>
2023-05-02 14:21:41 +0300

    first commit

---
+++ pythonProject/project/simpleapp/views.py
@@ -0,0 +1,25 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pub_date'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
